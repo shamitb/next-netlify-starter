f45a9adbd63df728295177620378955c1bfa9d0c
Shamit Bagchi <shamit@gmail.com>
2023-07-15 17:21:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,47 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Fantasy Adventure</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+    h1 {
+      text-align: center;
+      color: #333;
+    }
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 20px;
+    }
+    .button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #333;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 5px;
+    }
+    .button:hover {
+      background-color: #555;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Welcome to the Fantasy Adventure</h1>
+    <p>Choose your path:</p>
+    <div>
+      <a href="path1.html" class="button">Path 1</a>
+      <a href="path2.html" class="button">Path 2</a>
+      <a href="path3.html" class="button">Path 3</a>
     </div>
+  </div>
+</body>
+</html>
   )
 }
